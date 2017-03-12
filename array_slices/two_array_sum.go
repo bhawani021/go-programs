086b2fd5b015907383fdc71ac57ae895a2586801
bhawani <bhawani021@gmail.com>
2017-03-12 14:00:01 +0530

    Array: Sum of two array elements in go

---
+++ array_slices/two_array_sum.go
@@ -0,0 +1,17 @@
+// Sum of two array elememets
+package main
+
+import "fmt"
+
+func main() {
+	a := [5]int{10, 20, 30, 40, 50}
+	b := [5]int{100, 200, 300, 400, 500}
+
+	var c [5]int
+
+	for i := 0; i < 5; i++ {
+		c[i] = a[i] + b[i]
+	}
+
+	fmt.Println(c)
+}
