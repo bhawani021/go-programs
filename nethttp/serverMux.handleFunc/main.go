1fcb00e4093c0b340747ee43c582c53879e40cc2
bhawani021 <bhawani021@gmail.com>
2017-09-29 08:17:22 +0530

    serveMux.HandleFunc

---
+++ nethttp/serverMux.handleFunc/main.go
@@ -0,0 +1,24 @@
+// Use of serverMux.HandleFunc
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func customHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Use of serverMux.HandleFunc")
+}
+
+func main() {
+	// Create a serveMux object
+	mux := http.NewServeMux()
+
+	// Resigter URL path with handler using ServerMux.HandleFuc
+	mux.HandleFunc("/welcome", customHandler)
+
+	log.Println("Listening...")
+	// Create http server
+	http.ListenAndServe(":8080", mux)
+}
