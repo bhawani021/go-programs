2c8fce8105667b55d9e6308b7409c44185ac8f46
bhawani <bhawani021@gmail.com>
2017-11-01 12:26:07 +0530

    Delete a node using key

---
+++ linkedlist/linked_list_delete_using_key/main.go
@@ -0,0 +1,72 @@
+// Delete a node from linked list using key 
+
+package main
+
+import "fmt"
+
+
+type node struct {
+	Data int
+	Next *node
+}
+
+func displayNodes(n *node) {
+	for n != nil {
+		fmt.Print(n.Data, " ")
+		n = n.Next
+	}
+	fmt.Println("")
+}
+
+func deleteNode(n *node, key int) *node {
+	if n == nil {
+		return n
+	}
+
+	// If head node itself contains given key
+	if n.Data == key {
+		n = n.Next
+		return n
+	}
+
+	current := n
+	var prev *node
+	for current != nil {
+		if current.Data == key {
+			break
+		}
+		prev = current
+		current = current.Next
+		
+	}
+
+	// If key was not present in linked list
+	if current == nil {
+		return n
+	}
+
+	prev.Next = current.Next
+
+	return n
+}
+
+func main() {
+	// Create a new linked list
+	var n, current *node
+	n = &node{Data: 0, Next: nil}
+	current = n
+	for i := 5; i <= 50; i += 5 {
+		newNode := &node{Data: i, Next: nil}
+		current.Next = newNode
+		current = newNode
+	}
+
+	// Display linked list
+	displayNodes(n)
+
+	// Delete a node from linked list
+	n = deleteNode(n, 40)
+	// Print nodes after deletion
+	displayNodes(n)
+
+}
